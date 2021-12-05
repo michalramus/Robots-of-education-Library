38895a0b2ab6f46319f20fc75fde63a6b78a592e
Michał Ramus <ramus.michal21@gmail.com>
2021-12-05 12:04:00 +0100

    feat: Add XUnit project as unit test project

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -1,13 +1,14 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Xunit;
 
-namespace Tests
+namespace UnitTests
 {
-    [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [Fact]
+        public void Test1()
         {
+
         }
     }
 }
