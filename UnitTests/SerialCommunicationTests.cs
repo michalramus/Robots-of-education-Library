47c5577c44d349b5250a8ff0fb8b8ac0e5a8842b
Michał Ramus <ramus.michal21@gmail.com>
2021-12-11 14:43:25 +0100

    test: create unit tests for SerialCommunication.ReceiveMessage()
    -Receive correct message
    -Receive incorrect message
    -Serial port isn't open

--- UnitTests/SerialCommunicationTests.cs
+++ UnitTests/SerialCommunicationTests.cs
@@ -15,10 +15,12 @@ namespace SerialCommunicationTests
         [InlineData("{\"type\": \"task\",\"taskNum\": 1,\"task0\": {devType: < devType >, ID: < ID >, task: < task >, valNum: < num >, value1: [valueID, value], value2: [valueID, value]},}")]
         public void ReceiveMessage_ReceiveMessageAndRemoveStartEndMessageSymbol_GetMessage(string messageWithoutSymbol)
         {
+            //arrange
             //mock
             var serialPort = new Mock<ISerialPort>();
 
             serialPort.Setup(x => x.ReadLine()).Returns(SymbolsBase.GetSymbol(SymbolsIDs.startEndMessage) + messageWithoutSymbol + SymbolsBase.GetSymbol(SymbolsIDs.startEndMessage));
+            serialPort.Setup(x => x.IsOpen()).Returns(true);
             serialPort.SetupSequence(x => x.BytesToRead())
             .Returns(5) //higher than 0
             .Returns(0);
@@ -26,8 +28,52 @@ namespace SerialCommunicationTests
             //testing object
             var serialCommunication = new SerialCommunication(serialPort.Object);
 
-            //assert
+
+            //act & assert
             Assert.Equal(messageWithoutSymbol, serialCommunication.ReceiveMessage());
+        }
+
+        [Theory]
+        [InlineData("test")]
+        [InlineData("Message")]
+        [InlineData("space space")]
+        [InlineData("{\"type\": \"config\",\"devTypes\":[\"devx\", \"devy\"],\"devx\": 1,\"devy\": 2,\"devx0\": {\"ID\": 0, \"pins\": [pin1, pin2, pin3, pin4]},\"devy0\": {\"ID\": 0, \"pins\": [pin1, pin2, pin3, pin4]},\"devy1\": {\"ID\": 1, \"pins\": [pin1, pin2, pin3, pin4]},}")]
+        [InlineData("{\"type\": \"task\",\"taskNum\": 1,\"task0\": {devType: < devType >, ID: < ID >, task: < task >, valNum: < num >, value1: [valueID, value], value2: [valueID, value]},}")]
+        public void ReceiveMessage_ReceiveMessageWithoutStartEndMessageSymbol_ThrowException(string incorrectMessage)
+        {
+            //arrange
+            //mock
+            var serialPort = new Mock<ISerialPort>();
+
+            serialPort.Setup(x => x.ReadLine()).Returns(incorrectMessage);
+            serialPort.Setup(x => x.IsOpen()).Returns(true);
+            serialPort.SetupSequence(x => x.BytesToRead())
+            .Returns(5) //higher than 0
+            .Returns(0);
+
+            var serialCommunication = new SerialCommunication(serialPort.Object);
+           
+            //act & assert
+            IncorrectMessageException exception = Assert.Throws<IncorrectMessageException>(() => serialCommunication.ReceiveMessage());
+            //TODO: add exception data
+            Assert.Equal("aaa", exception.Message);
+        }
+
+        [Fact]
+        public void ReceiveMessage_SerialPortNotOpen_ThrowException()
+        {
+            //arrange
+            //mock
+            var serialPort = new Mock<ISerialPort>();
+
+            serialPort.Setup(x => x.IsOpen()).Returns(false);
+
+            var serialCommunication = new SerialCommunication(serialPort.Object);
+
+            //act & assert
+            InvalidOperationException exception = Assert.Throws<InvalidOperationException>(() => serialCommunication.ReceiveMessage());
+            //TODO: add exception data
+            Assert.Equal("aaa", exception.Message);
         }
     }
 }
