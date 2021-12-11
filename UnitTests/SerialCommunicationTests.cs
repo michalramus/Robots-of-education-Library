241b6400ac7bdc25113e9dff97743b97a2fdc079
Michał Ramus <ramus.michal21@gmail.com>
2021-12-11 18:21:53 +0100

    test: Create unit tests for SerialCommunication class

--- UnitTests/SerialCommunicationTests.cs
+++ UnitTests/SerialCommunicationTests.cs
@@ -7,6 +7,35 @@ namespace SerialCommunicationTests
 {
     public class SerialCommunicationTests
     {
+        //SendMessage
+
+        [Theory]
+        [InlineData("test")]
+        [InlineData("Message")]
+        [InlineData("space space")]
+        [InlineData("{\"type\": \"config\",\"devTypes\":[\"devx\", \"devy\"],\"devx\": 1,\"devy\": 2,\"devx0\": {\"ID\": 0, \"pins\": [pin1, pin2, pin3, pin4]},\"devy0\": {\"ID\": 0, \"pins\": [pin1, pin2, pin3, pin4]},\"devy1\": {\"ID\": 1, \"pins\": [pin1, pin2, pin3, pin4]},}")]
+        [InlineData("{\"type\": \"task\",\"taskNum\": 1,\"task0\": {devType: < devType >, ID: < ID >, task: < task >, valNum: < num >, value1: [valueID, value], value2: [valueID, value]},}")]
+        public void SendMessage_SendCorrectMessage_ReturnMessage(string message)
+        {
+            //arrange
+            string startEndSymbol = SymbolsBase.GetSymbol(SymbolsIDs.startEndMessage);
+            var serialPort = new Mock<ISerialPort>();
+            serialPort.Setup(x => x.Write(It.IsAny<string>()));
+            
+
+            SerialCommunication serialCommunication = new SerialCommunication(serialPort.Object);
+
+            //act
+            serialCommunication.SendMessage(message);
+
+            //assert
+            serialPort.Verify(x => x.Write(startEndSymbol + message + startEndSymbol), Times.Once);
+        }
+
+
+
+        // ReceiveMessage
+
         [Theory]
         [InlineData("test")]
         [InlineData("Message")]
@@ -52,7 +81,7 @@ namespace SerialCommunicationTests
             .Returns(0);
 
             var serialCommunication = new SerialCommunication(serialPort.Object);
-           
+
             //act & assert
             IncorrectMessageException exception = Assert.Throws<IncorrectMessageException>(() => serialCommunication.ReceiveMessage());
             //TODO: add exception data
