07bdb97363c787032531def6ca28604473cdc249
Michał Ramus <ramus.michal21@gmail.com>
2021-12-18 17:06:23 +0100

    feat: add new symbols to RobotSymbols base

--- Library/Symbols/RobotsSymbols/ERobotsSymbols.cs
+++ Library/Symbols/RobotsSymbols/ERobotsSymbols.cs
@@ -1,6 +1,15 @@
 enum ERobotsSymbols
 {
     //Types of robots and values of it
+
+    //universal symbols
+    devType,
+    devID,
+    pins,
+
+    //symbols for concrete robot
+
+    //car
     car,
 
 }
--- Library/Symbols/RobotsSymbols/RobotsSymbols.cs
+++ Library/Symbols/RobotsSymbols/RobotsSymbols.cs
@@ -8,6 +8,14 @@ namespace ROELibrary
         public Symbols<ERobotsSymbols> symbols = new Symbols<ERobotsSymbols>(
             new Dictionary<ERobotsSymbols, string>
             {
+                //universal symbols
+                {ERobotsSymbols.devType, "type"},
+                {ERobotsSymbols.devID, "ID"},
+                {ERobotsSymbols.pins, "pins"},
+
+                //symbols for concrete robot
+                
+                //car
                 {ERobotsSymbols.car, "car"},
             }
         );
