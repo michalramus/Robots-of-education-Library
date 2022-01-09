dd2317bccaf01c05864fc16fb07a0416d73bbdcd
Michał Ramus <ramus.michal21@gmail.com>
2022-01-09 13:58:57 +0100

    ref: mark all properties in CarModel as virtual
    Note that it is necessary to mock CarModel

--- Library/Robots/Car/CarModel.cs
+++ Library/Robots/Car/CarModel.cs
@@ -3,26 +3,26 @@ namespace ROELibrary
     public class CarModel : VRobotModel
     {
         //properties
-        public uint? id { get; set; } = null;
+        public virtual uint? id { get; set; } = null;
 
         /// <summary>
         /// [en1][en2][en3][en4][speedControl-analogPin]
         /// </summary>
         /// <value></value>
-        public uint[] pins { get; set; } = null;
+        public virtual uint[] pins { get; set; } = null;
 
         //calibration properties
         /// <summary>
         /// impulses sent by hall sensor per one wheel rotation
         /// </summary>
         /// <value></value>
-        public uint? impulsesPerRotation { get; set; } = null;
+        public virtual uint? impulsesPerRotation { get; set; } = null;
 
         /// <summary>
         /// circumference of wheel in centimeters
         /// </summary>
         /// <value></value>
-        public uint? circumference { get; set; } = null;
+        public virtual uint? circumference { get; set; } = null;
 
         internal override ERobotsSymbols getModelType()
         {
