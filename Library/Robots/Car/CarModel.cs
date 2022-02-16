29af693bafbf6e46b4959d968fe4b9e2a331f26e
Michal123456747 <ramus.michal21@gmail.com>
2022-02-16 09:07:19 +0100

    feat: set properties in CarModel to internal
    Disable every settings of Car Thanks this library is compatible with
    prevew formware for microcontrollers

--- Library/Robots/Car/CarModel.cs
+++ Library/Robots/Car/CarModel.cs
@@ -2,37 +2,39 @@ namespace ROELibrary
 {
     public class CarModel : VRobotModel
     {
+        //TODO: set every property to null and public
+
         //properties
-        public virtual uint? id { get; set; } = null;
+        internal virtual uint? id { get; set; } = 0;
 
         /// <summary>
         /// [en1][en2][en3][en4][speedControl-analogPin]
         /// </summary>
         /// <value></value>
-        public virtual uint[] pins { get; set; } = null;
+        internal virtual uint[] pins { get; set; } = { 1, 2, 3, 4, 5 };
 
         //calibration properties
         /// <summary>
         /// impulses sent by hall sensor per one wheel rotation
         /// </summary>
         /// <value></value>
-        public virtual uint? impulsesPerRotation { get; set; } = null;
+        internal virtual uint? impulsesPerRotation { get; set; } = 50;
 
         /// <summary>
         /// circumference of wheel in centimeters
         /// </summary>
         /// <value></value>
-        public virtual uint? circumference { get; set; } = null;
+        internal virtual uint? circumference { get; set; } = 30;
 
         internal override ERobotsSymbols getModelType()
         {
             return ERobotsSymbols.car;
         }
 
-    /// <summary>
-    /// return null when id not set
-    /// </summary>
-    /// <returns></returns>
+        /// <summary>
+        /// return null when id not set
+        /// </summary>
+        /// <returns></returns>
         internal override uint? getID()
         {
             return id;
